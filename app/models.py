e3bd7f7638033441af301d790585771b950cceba
avict15 <avinashroepa@gmail.com>
2019-05-13 16:07:00 +0200

    Update models.py
    Kaasje

--- app/models.py
+++ app/models.py
@@ -3,6 +3,8 @@ from app import db, login
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 
+Ewa dreddrie
+
 class Chargingpoint(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     price = db.Column(db.Integer)
